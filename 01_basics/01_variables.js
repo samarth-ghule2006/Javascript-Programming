e1fdb3b31ef6fa921364b365f0d6da65a78ae819
Samarth Ghule <samarthghule2006@gmail.com>
2026-04-13 20:50:36 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+
+let name = "Samarth";
+var age = 20;
+const country = "India";
+
+console.log(name);
+console.log(age);
+console.log(country);
+
+// changing value
+age = 21;
+console.log(age);
+
+// country = "Dubai" // Cannot change the value
